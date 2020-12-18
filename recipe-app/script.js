a824f41b74b7c75e6d61110ab1b4d9c44635b50c
Chris Ference <cference8@gmail.com>
2020-12-17 20:34:57 -0600

    done

--- recipe-app/script.js
+++ recipe-app/script.js
@@ -1,5 +1,8 @@
 const mealsEl = document.getElementById('meals');
 const favoriteContainer = document.getElementById('fav-meals');
+const mealPopup = document.getElementById('meal-popup');
+const mealInfoEl = document.getElementById('meal-info');
+const popupCloseBtn = document.getElementById('close-popup');
 const searchTerm = document.getElementById('search-term');
 const searchBtn = document.getElementById('search');
 getRandomMeal();
@@ -70,6 +73,10 @@ function addMeal(mealData, random = false) {
     fetchFavMeals();
   });
 
+  meal.addEventListener('click', () => {
+    showMealInfo(mealData);
+  });
+
   mealsEl.appendChild(meal);
 }
 
@@ -125,7 +132,57 @@ function addFavMeal(mealData) {
 
     fetchFavMeals();
   });
+
+  favMeal.addEventListener('click', () => {
+    showMealInfo(mealData);
+  });
+
   favoriteContainer.appendChild(favMeal);
+}
+
+function showMealInfo(mealData) {
+  //clear it
+  mealInfoEl.innerHTML = '';
+  //update
+  const mealEl = document.createElement('div');
+
+  const ingredients = [];
+
+  //get indredients
+  for (let i = 1; i < 20; i++) {
+    if (mealData['strIngredient' + i]) {
+      ingredients.push(
+        `${mealData['strIngredient' + i]} - 
+        ${mealData['strMeasure' + i]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  mealEl.innerHTML = `
+  <h1>${mealData.strMeal}</h1>
+  <img
+    src="${mealData.strMealThumb}"
+    alt="${mealData.strMeal}"
+  />
+  <p>
+  ${mealData.strInstructions}
+  </p>
+  <h3>Ingredients</h3>
+  <ul>
+    ${ingredients
+      .map(
+        (ing) => `
+    <li>${ing}</li>
+    `
+      )
+      .join('')}
+  </ul>
+  `;
+  mealInfoEl.appendChild(mealEl);
+  //show popup
+  mealPopup.classList.remove('hidden');
 }
 
 searchBtn.addEventListener('click', async () => {
@@ -140,4 +197,8 @@ searchBtn.addEventListener('click', async () => {
       addMeal(meal);
     });
   }
+});
+
+popupCloseBtn.addEventListener('click', () => {
+  mealPopup.classList.add('hidden');
 });
